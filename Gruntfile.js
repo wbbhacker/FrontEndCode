f77fe2e42e91abbdcce4017a626929f538a6db61
xiaobin <2404016117@qq.com>
2016-04-13 19:01:26 +0800

    node

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch:{
+       
     	Questions:{
-    		files:'Questions/*.*'
+    		files:'javascript/ajax/*.*',
+            options: {
+              livereload: 35729
+            }
     	}
+    },
+    connect: {
+        
+        server: {
+            port:8000,
+            options: {
+              livereload: 35729
+            }
+        }
     }
  
   });
 
   // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // 默认被执行的任务列表。
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['connect','watch']);
 
 };
