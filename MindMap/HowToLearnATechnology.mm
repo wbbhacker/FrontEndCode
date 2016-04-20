2fc54b662f008973f6edf9735249150ef9ad5392
xiaobin <2404016117@qq.com>
2016-04-20 17:42:35 +0800

    mind

---
+++ MindMap/HowToLearnATechnology.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1461124965895" ID="ID_1267523932" MODIFIED="1461125134437" TEXT="&#x6280;&#x672f;">
+<node CREATED="1461125001563" ID="ID_66946578" MODIFIED="1461125130508" POSITION="right" TEXT="&#x4f7f;&#x7528;&#x65b9;&#x6cd5;"/>
+<node CREATED="1461125039386" ID="ID_1194638441" MODIFIED="1461125122496" POSITION="right" TEXT="&#x89e3;&#x51b3;&#x90a3;&#x4e9b;&#x95ee;&#x9898;&#x53ca;&#x5e94;&#x7528;&#x573a;&#x666f;  "/>
+<node CREATED="1461125061162" ID="ID_1111645633" MODIFIED="1461125074689" POSITION="right" TEXT="&#x597d;&#x5904;&#x548c;&#x574f;&#x5904;"/>
+<node CREATED="1461125080307" ID="ID_1331966400" MODIFIED="1461125087361" POSITION="right" TEXT="&#x7c7b;&#x4f3c;&#x6280;&#x672f;"/>
+</node>
+</map>
