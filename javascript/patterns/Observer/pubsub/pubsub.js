66be6f70003e5a9c6777d8cb475fc767055d31a3
binbinwang <2404016117@qq.com>
2016-05-02 23:59:52 +0800

    a

---
+++ javascript/patterns/Observer/pubsub/pubsub.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+				
+			subUid = -1;
+		p.prototype.publisher = function(){
+		var topics = {},
+		}
+	(function(p){
+	return pubsub;
+	var pubsub = {};
+	})(pubsub)
+define(function(channel){
+})
