6d0df8d68bc63fb7705f44358059d1b2d3db2bd6
xiaobin <2404016117@qq.com>
2016-05-03 14:01:29 +0800

    publish/subscribe

--- javascript/patterns/Observer/pubsub/pubsub.js
+++ javascript/patterns/Observer/pubsub/pubsub.js
@@ -1,17 +1,59 @@
+define(function() {
+    var pubsub = {};
+
+    (function(p) {
+        var topics = {},
+            subUid = -1;
 
 
 
+        p.publish = function(topic, args) {
+
+            if (!topics[topic]) {
+                return false;
+            }
+
+            var subscribes = topics[topic]; 
+            len = subscribes.length;
+      
+            while ( --len >= 0 ) {
+                topics[topic][len].func(args);
+            }
+
+        };
+
+        p.subscribe = function(topic, func) {
+        	if (!topics[topic]) {
+        		topics[topic] = [];
+        	}
+            var token = (++subUid).toString();
+
+            topics[topic].push({
+                token: token,
+                func: func
+            });
+
+            return token;
+
+        };
+
+        p.unsubscribe = function(token) {
+            for (var n in topics) {
+            	var	i = 0, 
+            		len = topics[n].length;
+            	for ( ;i<len; i++) {
+            		if (topics[n][i].token === token) {
+            			topics[n].splice(i,1);
+            			return token;
+            		}
+            	}
+            }
+        };
+
+        
+    })(pubsub)
 
 
 
-				
-			subUid = -1;
-		p.prototype.publisher = function(){
-		var topics = {},
-		}
-	(function(p){
-	return pubsub;
-	var pubsub = {};
-	})(pubsub)
-define(function(channel){
+    return pubsub;
 })
