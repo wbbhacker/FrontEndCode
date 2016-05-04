a0a93e9bd026b112db82a8d747f8c562a825f00d
xiaobin <2404016117@qq.com>
2016-05-04 17:13:10 +0800

    js

--- javascript/jsFn.js
+++ javascript/jsFn.js
@@ -31,7 +31,46 @@ _.isArray = nativeIsArray || function(obj){
 };
 
 
-// 
+// 深度克隆
+
+function clone(obj)
+{
+    var o,i,j,k;
+    if(typeof(obj)!="object" || obj===null)return obj;
+    if(obj instanceof(Array))
+    {
+        o=[];
+        i=0;j=obj.length;
+        for(;i<j;i++)
+        {
+            if(typeof(obj[i])=="object" && obj[i]!=null)
+            {
+                o[i]=arguments.callee(obj[i]);
+            }
+            else
+            {
+                o[i]=obj[i];
+            }
+        }
+    }
+    else
+    {
+        o={};
+        for(i in obj)
+        {
+            if(typeof(obj[i])=="object" && obj[i]!=null)
+            {
+                o[i]=arguments.callee(obj[i]);
+            }
+            else
+            {
+                o[i]=obj[i];
+            }
+        }
+    }
+ 
+    return o;
+}
 
 
 
