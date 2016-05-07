7d38d67dbd15fbe12f60b6a94ca1fe0acfef1c40
binbinwang <2404016117@qq.com>
2016-05-07 18:46:26 +0800

    dropDown

--- javascript/plug/formdemo/dropDown.js
+++ javascript/plug/formdemo/dropDown.js
@@ -1,3 +1,5 @@
+var num = 0;
+
 var dropDown = function(o){
 	// default options
 	var defaults = {};
@@ -14,38 +16,45 @@ var dropDown = function(o){
 
 	this.stateEdit = false;
 	this.stateDrop = false;
+	this.phValue = '';
+	this.pvValue = '';
 	this.value = '';
-	this.prevValue = $visibleValue;
 
+	//  多个下拉选项出现错位
 
+	num += 1;
+	$(this.o.op).css('z-index',num);
+	
 	$options.click(function(){
 		var tValue = $(this).attr('data-value');
 		_slef.close();
 		$visibleValue.text($(this).text());
 		$hiddenValue.val(tValue);
-
-
 		_slef.value = tValue;
-		console.log(_slef)
 	});
+
 
 	$fnBtn.click(function(){
 		_slef.stateDrop ? _slef.close() : _slef.open();
 	});
 
+
 }
-
-
 dropDown.prototype = {
 	constructor:dropDown,
 	edit:function(){
 		this.o.dropStyle();
 		this.stateEdit = true;
+		this.phValue = $(this.o.hC).val();
+		this.pvValue = $(this.o.vC).text();
 	},
 	cancelEdit:function(){
 		this.o.normalStyle();
 		this.close();
 		this.stateEdit = false;
+		$(this.o.hC).val(this.phValue);
+		$(this.o.vC).text(this.pvValue);
+		this.value = this.phValue;
 	},
 	confirmEdit:function(){
 		this.o.normalStyle();
