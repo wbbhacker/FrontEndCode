221898991d3b98f0aad975e285b04ffd775fa3a0
xiaobin <2404016117@qq.com>
2016-05-17 15:39:12 +0800

    fix dropDown.js

--- javascript/plug/formdemo/dropDown.js
+++ javascript/plug/formdemo/dropDown.js
@@ -1,74 +1,95 @@
-var num = 0;
-
 var dropDown = function(o){
-	// default options
-	var defaults = {};
+				// default options
+				var defaults = {};
 
-	o = $.extend({},defaults,o);
+				o = $.extend({},defaults,o);
 
-	var $options = $(o.oC),
-		$visibleValue = $(o.vC),
-		$hiddenValue = $(o.hC),
-		$fnBtn = $(o.cC),
-		_slef = this;
+				var $options = $(o.oC),
+					$visibleValue = $(o.vC),
+					$hiddenValue = $(o.hC),
+					$fnBtn = $(o.cC),
+					_slef = this;
 
-	this.o = o;
+				this.o = o;
 
-	this.stateEdit = false;
-	this.stateDrop = false;
-	this.phValue = '';
-	this.pvValue = '';
-	this.value = '';
-
-	//  多个下拉选项出现错位
-
-	num += 1;
-	$(this.o.op).css('z-index',num);
-	
-	$options.click(function(){
-		var tValue = $(this).attr('data-value');
-		_slef.close();
-		$visibleValue.text($(this).text());
-		$hiddenValue.val(tValue);
-		_slef.value = tValue;
-	});
+				this.stateEdit = false;
+				this.stateDrop = false;
+				this.phValue = '';
+				this.pvValue = '';
+				this.value = '';
 
 
-	$fnBtn.click(function(){
-		_slef.stateDrop ? _slef.close() : _slef.open();
-	});
+
+				$options.click(function(){
+					var tValue = $(this).attr('data-value');
+					_slef.close();
+					$visibleValue.text($(this).text());
+					$hiddenValue.val(tValue);
+					_slef.value = tValue;
+					if(o.callback) 
+						o.callback(tValue);
+				});
+
+				$fnBtn.click(function(){
+					_slef.stateDrop ? _slef.close() : _slef.open();
+				});
+
+				
+
+				$('body').click(bindclose);
+				
+
+			    function bindclose(){
+			    	console.log("body click");
+					_slef.close();
+				}
 
 
-}
-dropDown.prototype = {
-	constructor:dropDown,
-	edit:function(){
-		this.o.dropStyle();
-		this.stateEdit = true;
-		this.phValue = $(this.o.hC).val();
-		this.pvValue = $(this.o.vC).text();
-	},
-	cancelEdit:function(){
-		this.o.normalStyle();
-		this.close();
-		this.stateEdit = false;
-		$(this.o.hC).val(this.phValue);
-		$(this.o.vC).text(this.pvValue);
-		this.value = this.phValue;
-	},
-	confirmEdit:function(){
-		this.o.normalStyle();
-		this.close();
-		this.stateEdit = false;
-	},
-	open:function(){
-		if(!this.stateEdit) return;
-		this.o.openStyle();
-		this.stateDrop = true;
-	},
-	close:function(){
-		if(!this.stateEdit) return;
-		this.o.closeStyle();
-		this.stateDrop = false;
-	}
-} 
+			}
+			dropDown.prototype = {
+				constructor:dropDown,
+				edit:function(){
+					this.o.dropStyle();
+					this.stateEdit = true;
+					this.phValue = $(this.o.hC).val();
+					this.pvValue = $(this.o.vC).text();
+				},
+				cancelEdit:function(){
+					this.o.normalStyle();
+					this.close();
+					this.stateEdit = false;
+					$(this.o.hC).val(this.phValue);
+					$(this.o.vC).text(this.pvValue);
+					this.value = this.phValue;
+				},
+				confirmEdit:function(){
+					this.o.normalStyle();
+					this.close();
+					this.stateEdit = false;
+				},
+				open:function(){
+					var _slef = this;
+
+					// setTimeout 是为了解决两个同时存在时，先让它冒泡，在禁止它冒泡
+					setTimeout(function(){
+
+						if(!_slef.stateEdit) return;
+						_slef.o.openStyle();
+						_slef.stateDrop = true;
+						$(_slef.o.op).click(_slef.stopPor);
+
+					},10);
+
+				},
+				close:function(){
+					var _slef = this;
+					if(!this.stateEdit) return;
+					this.o.closeStyle();
+					this.stateDrop = false;
+					$(this.o.op).unbind('click',this.stopPor);
+
+				},
+				stopPor:function(e){
+					e.stopPropagation();
+				}
+			} 
